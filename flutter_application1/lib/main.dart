11f981509d5f13b35392bed2f126ca3dada7ae9f
JohnAdmin <teerasak.john@gmail.com>
2022-03-26 16:22:39 +0700

    comit

--- flutter_application1/lib/main.dart
+++ flutter_application1/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo by John',
+      title: 'Flutter Demo by John 2021-abc9999',
       theme: ThemeData(
         // This is the theme of your application.
         //
